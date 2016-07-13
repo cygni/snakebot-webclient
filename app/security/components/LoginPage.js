dbf8fe7df26e279b6d03c2697a0bff2ba3ba4622
Daniel Lenksjo <Daniel Lenksjo>
2016-07-13 22:21:09 +0200

    Removed intermediate step when signing out.
    Remove the intermediate step when clicking the sign-out link. Now we are
    signing out immediately. Also made use of ES6 string interpolation in the
    AuthService.
    
    After reading up on
    https://facebook.github.io/react/docs/reusable-components.html#no-autobinding
    I did the binding for event handlers in the constructor instead of in the
    JSX. This hardly matters for us, but I thought it made sense to keep the
    JSX as clean as possible and to follow React recommendation.
    
    This closes #7

--- app/security/components/LoginPage.js
+++ app/security/components/LoginPage.js
@@ -1,48 +1,51 @@
 import React from "react";
 import {Row, Col, Input, ButtonInput} from "react-bootstrap";
-import Auth from '../services/AuthService';
+import AuthService from '../services/AuthService';
 import Store from '../../baseStore/BaseStore'
 
 class LoginPage extends React.Component {
-
     constructor(props) {
         super(props);
-        this.state = {
-            user: '',
-            password: ''
-        };
+        this.auth = new AuthService();
+        this.state = { user: '', password: '' };
+
+        this.login = this.login.bind(this);
+        this.logout = this.logout.bind(this);
+        this.handleUserChange = this.handleUserChange.bind(this);
+        this.handlePasswordChange = this.handlePasswordChange.bind(this);
     }
 
     login(e) {
         e.preventDefault();
-        Auth.login(this.state.user, this.state.password)
+        this.auth.login(this.state.user, this.state.password)
             .catch(function (err) {
-                alert("There's an error logging in");
+                alert("There was an error logging in");
                 console.log("Error logging in", err);
             });
     }
 
+    logout(e) {
+        this.auth.logout();
+    }
+
     handleUserChange(e) {
         this.setState({
-                user: e.target.value
-            }
-        );
+            user: e.target.value
+        });
     }
 
     handlePasswordChange(e) {
         this.setState({
-                password: e.target.value
-            }
-        );
+            password: e.target.value
+        });
     }
 
     render() {
-
         if (Store.isLoggedIn()) {
             return (
                 <Row>
                     <Col xs={10} md={6}>
-                        <form role="form" onSubmit={Auth.logout}>
+                        <form role="form" onSubmit={this.logout}>
                             <ButtonInput type="submit" value="Sign out"/>
                         </form>
                     </Col>
@@ -53,14 +56,14 @@ class LoginPage extends React.Component {
         return (
             <Row>
                 <Col xs={18} md={12}>
-                    <h1> Sign in</h1>
+                    <h1>Sign in</h1>
                 </Col>
                 <Col xs={10} md={6}>
-                    <form role="form" onSubmit={this.login.bind(this)}>
-                        <Input value={this.state.tempGameName} onChange={this.handleUserChange.bind(this)} type="text"
-                               label="Username" placeholder="username"/>
-                        <Input value={this.state.tempGameName} onChange={this.handlePasswordChange.bind(this)} type="text"
-                               label="Password" type="password" placeholder="password"/>
+                    <form role="form" onSubmit={this.login}>
+                        <Input value={this.state.user} onChange={this.handleUserChange} type="text"
+                            label="Username" placeholder="username"/>
+                        <Input onChange={this.handlePasswordChange} type="text"
+                            label="Password" type="password" placeholder="password"/>
                         <ButtonInput type="submit" value="Sign in"/>
                     </form>
                 </Col>
