b0ee134373ead8a5ff0e770eb993de6d6b00c50e
David Schlyter <dschlyter@gmail.com>
2016-10-04 21:53:55 +0200

    Make the placeholder text great again

--- app/pages/HomePage.jsx
+++ app/pages/HomePage.jsx
@@ -4,19 +4,26 @@ import '../design/styles/stylesheet.scss';
 function HomePage() {
   return (
     <section className="clear-fix information">
-      <p>
-        Nulla nec lectus vel erat.
-        Sed sit amet magna ac ipsum sagittis consectetur at ac magna.
-        Aliquam erat volutpat.
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        Duis aliquam eros non elit efficitur, ut euismod sapien eleifend.
-        Curabitur sodales enim lacinia orci congue convallis.
-        Nulla nec lectus vel erat venenatis finibus a at nisi.
-        In ac leo mattis, dapibus velit at, gravida diam.
-        Suspendisse ultrices maximus facilisis.
-        Sed sit amet magna ac ipsum  sagittis consectetur at ac magna.
-        Aliquam erat volutpat. Nunc eget augue quis lectus
-      </p>
+      <p>Lorem Ipsum's father was with Lee Harvey Oswald prior to Oswald's being, you know, shot.
+      You have so many different things placeholder text has to be able to do, and I don't
+      believe Lorem Ipsum has the stamina.</p>
+
+      <p>We have so many things that we have to do better... and certainly ipsum is one of them.
+      It’s about making placeholder text great again. That’s what people want, they want
+      placeholder text to be great again.</p>
+
+      <p>I'm speaking with myself, number one, because I have a very good brain and I've said
+      a lot of things. I have a 10 year old son. He has words. He is so good with these words
+      it's unbelievable.</p>
+
+      <p>You’re disgusting. An 'extremely credible source' has called my office and told me that
+      Lorem Ipsum's birth certificate is a fraud. Trump Ipsum is calling for a total and
+      complete shutdown of Muslim text entering your website. An ‘extremely credible source’
+      has called my office and told me that Barack Obama’s placeholder text is a fraud.</p>
+
+      <p>An ‘extremely credible source’ has called my office and told me that Barack Obama’s
+      placeholder text is a fraud. I think my strongest asset maybe by far is my temperament.
+      I have a placeholding temperament. Lorem Ispum is a choke artist. It chokes!</p>
     </section>
   );
 }
