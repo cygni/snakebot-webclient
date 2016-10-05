a1d56933081be89b9158a7e6c154307f0f9dd2f3
Martin Barksten <martin.barksten@gmail.com>
2016-10-05 10:39:01 +0200

    Ensure high score list is sorted

--- app/baseStore/BaseStore.js
+++ app/baseStore/BaseStore.js
@@ -194,6 +194,7 @@ const _tournamentCreated = (tournamentInfo) => {
 const _tournamentEnded = (event) => {
   console.log('Tournament ended', tournament, event);
   tournament.finalPlacement.list = event.gameResult;
+  tournament.finalPlacement.list.sort((s1, s2) => s2.points - s1.points);
   tournament.finalPlacement.winner =
     tournament.finalPlacement.list.find(
       snake => snake.playerId === event.playerWinnerId);
