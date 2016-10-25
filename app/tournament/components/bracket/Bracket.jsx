03d48235cf967ff1e8747d1e06d33266124e855b
Martin Barksten <martin.barksten@gmail.com>
2016-10-25 19:36:34 +0200

    Begin restyling the brackets page

--- app/tournament/components/bracket/Bracket.jsx
+++ app/tournament/components/bracket/Bracket.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
 
 import TournamentStore from '../../../baseStore/BaseStore';
 import StoreWatch from '../../watch/StoreWatch';
 import TournamentAction from '../../action/tournament-actions';
-import FinalPlacementList from '../players/FinalPlacementList';
 
 function getGamePlan() {
   const gamePlan = TournamentStore.getTournamentGamePlan();
@@ -22,6 +20,21 @@ class Bracket extends React.Component {
     return () => TournamentAction.setActiveTournamentGame(game.gameId);
   }
 
+  static roundClassName(round) {
+    switch (round.level) {
+      case 0:
+        return 'first-round';
+      case 1:
+        return 'second-round';
+      case 2:
+        return 'third-round';
+      case 3:
+        return 'final-round';
+      default:
+        return '';
+    }
+  }
+
   render() {
     console.log('Rendering Bracket', this.props);
     if (!this.props.gamePlan) {
@@ -29,34 +42,35 @@ class Bracket extends React.Component {
     }
 
     return (
-      <main id="tournament"> {
-        this.props.gamePlan.tournamentLevels.map((level, i) => (
-          <div className="panel panel-info col-sm-3" key={i}>
-            <div className="panel-heading">Round {level.level}</div>
-            { level.tournamentGames.map((game, j) => (
-              <div
-                className="panel-body"
-                key={j}
-              >
-                <h4> Game {j} </h4>
-                <ul className="list-group player"> {
-                  game.players.map(player => (
-                    <li
-                      className="list-group-item"
-                      key={player.id}
-                    >{player.name} </li>))}
-                </ul>
-                <Button
-                  onClick={Bracket.chooseGame(game)}
-                  bsStyle="info"
-                  bsSize="large"
-                >Go to game
-                </Button>
-              </div>))}
-          </div>
-        ))}
-        <FinalPlacementList />
-      </main>
+      <section className="page clear-fix">
+        <article>
+          <h1>Tournament</h1>
+        </article>
+        <div className="tournament"> {
+          this.props.gamePlan.tournamentLevels.reverse().map((level, i) => (
+            <div key={i}>
+              <div className={Bracket.roundClassName(level) + ' round-box'}>
+                <h2>Round {level.level}</h2>
+                <div className="flex">
+                  { level.tournamentGames.map((game, j) => (
+                    <ul className="game" key={j}> {
+                      game.players.map(player => (
+                        <li
+                          key={player.id}
+                        >{player.name} </li>))}
+                      <button
+                        type="button"
+                        onClick={Bracket.chooseGame(game)}
+                      >Go to game</button>
+                    </ul>
+                    ))}
+                </div>
+              </div>
+              { level.level > 0 ? <div className="spacer" /> : <div /> }
+            </div>
+          ))}
+        </div>
+      </section>
     );
   }
 }
