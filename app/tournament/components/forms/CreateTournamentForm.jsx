ea00e7808a0de00a2e49a542ad4d1a0f5e6889e1
Johannes Dolk <johannes.dolk@cygni.se>
2016-11-01 18:14:57 +0100

    removed unsued import

--- app/tournament/components/forms/CreateTournamentForm.jsx
+++ app/tournament/components/forms/CreateTournamentForm.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Row, Col } from 'react-bootstrap';
 import TournamentAction from '../../action/tournament-actions';
 
 class CreateTournamentForm extends React.Component {
