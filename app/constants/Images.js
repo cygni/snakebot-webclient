d776e8c5b1683d8b4f359726a55f0ed4c0337250
jonas.ohrn <jonas.ohrn@maxm.se>
2016-11-27 23:21:06 +0100

    Updated issue #152 - Fixed rendering of dead snakes to use same code as normal snakes with alpha on body and head/tail images. Improved line rendering at edges. Restored .eslintrc file to original.

--- app/constants/Images.js
+++ app/constants/Images.js
@@ -21,18 +21,9 @@ import snake000000Tail from '../design/images/snakes/000000/000000_TAIL.png';
 import snakeFF4848Tail from '../design/images/snakes/FF4848/FF4848_TAIL.png';
 import snake9AF48ETail from '../design/images/snakes/9AF48E/9AF48E_TAIL.png';
 import snake9BF3F0Tail from '../design/images/snakes/9BF3F0/9BF3F0_TAIL.png';
-// 50% alpha
-import deadSnakeTail50 from '../design/images/snakes/999999/grey-dead-tail-50.svg';
-import deadSnakeHead50 from '../design/images/snakes/999999/grey-dead-head-50.svg';
-import deadSnakeBody50 from '../design/images/snakes/999999/grey-dead-body-50.svg';
-// 70% alpha
-import deadSnakeTail70 from '../design/images/snakes/999999/grey-dead-tail-70.svg';
-import deadSnakeHead70 from '../design/images/snakes/999999/grey-dead-head-70.svg';
-import deadSnakeBody70 from '../design/images/snakes/999999/grey-dead-body-70.svg';
 // 100% alpha
 import deadSnakeTail100 from '../design/images/snakes/999999/grey-dead-tail-100.svg';
 import deadSnakeHead100 from '../design/images/snakes/999999/grey-dead-head-100.svg';
-import deadSnakeBody100 from '../design/images/snakes/999999/grey-dead-body-100.svg';
 
 const starImg = star;
 
@@ -60,9 +51,9 @@ function _getSnakeHead(color) {
       return snake9AF48E;
     case '#9BF3F0' :
       return snake9BF3F0;
-    case '#dead' : {
+    case '#dead' :
+    case '#999999' :
       return deadSnakeHead100;
-    }
     default:
       return snake000000;
   }
@@ -92,43 +83,16 @@ function _getSnakeTail(color) {
       return snake9AF48ETail;
     case '#9BF3F0' :
       return snake9BF3F0Tail;
-    default:
-      return snake000000Tail;
-  }
-}
-
-function _getDeadImage(part, alpha) {
-  switch (part) {
-    case 'head':
-      if (alpha === 70) {
-        return deadSnakeHead70;
-      } else if (alpha === 50) {
-        return deadSnakeHead50;
-      }
-      return deadSnakeHead100;
-    case 'tail':
-      if (alpha === 70) {
-        return deadSnakeTail70;
-      } else if (alpha === 50) {
-        return deadSnakeTail50;
-      }
+    case '#999999' :
       return deadSnakeTail100;
     default:
-      if (alpha === 70) {
-        return deadSnakeBody70;
-      } else if (alpha === 50) {
-        return deadSnakeBody50;
-      }
-      return deadSnakeBody100;
+      return snake000000Tail;
   }
 }
 
 export default {
   getStarImage(position) {
     return { src: starImg, key: 'star_' + position };
-  },
-  getDeadImage(part, alpha) {
-    return { src: _getDeadImage(part, alpha), key: undefined };
   },
   getSnakeHead(color) {
     return { src: _getSnakeHead(color), key: 'head_' + color };
