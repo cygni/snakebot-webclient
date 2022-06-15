762593419fabe548153dbb97ab3d0f800a97e912
Sebastian <61822376+SebastianH-ctrl@users.noreply.github.com>
2022-06-15 09:56:58 +0200

    Configuring webclient
    webpack & package.json
    
    also manually added .babelrc file

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,15 @@
 const versionNumber = require('./package.json').version;
 const argv = require('minimist')(process.argv.slice(2));
 const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const getServerUrl = function () {
-  const host = argv['server-host'] !== undefined ? argv['server-host'] : 'snake.cygni.se';
+  /*const host = argv['server-host'] !== undefined ? argv['server-host'] : 'snake.cygni.se';
   if (argv['server-port'] !== undefined) {
     return 'http://' + host + ':' + argv['server-port'];
-  }
-  return 'http://' + host;
+  }*/
+  return 'http://localhost:8080';
 };
 
 const getEnvironment = function () {
@@ -35,57 +37,35 @@ console.log('Building version: ' + configuration.version);
 console.log('Using snake server host: ' + configuration.server);
 
 module.exports = {
+  mode: 'production',
   resolve: {
     extensions: ['', '.js', '.jsx'],
   },
   entry: './app/App.jsx',
   output: {
-    path: './dist',
+    path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
     publicPath: '/',
   },
-  devServer: {
-    inline: true,
-    contentBase: './dist',
-  },
+  
   plugins: [
     new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify(configuration.environment),
-      },
-    }),
+      'process.env.NODE_ENV' : JSON.stringify('production')
+  }),
+    
+      new HtmlWebpackPlugin({
+        template: 'dist/index.html'
+      })
   ],
   module: {
-    preLoaders: [{
-      test: /\.(js|jsx)$/,
-      loader: 'eslint-loader',
-      cache: true,
-    }],
-    loaders: [{
-      test: /\.jsx?$/,
+    rules: [{
+      test: /\.jsx?/,
       exclude: /node_modules/,
-      loader: 'babel',
-      query: {
-        presets: ['react', 'es2015'],
-        plugins: prod() ? ['transform-remove-console'] : [],
-      },
-    }, {
-      test: /\.scss$/,
-      include: /app/,
-      loaders: ['style', 'css', 'sass'],
-    }, {
-      test: /\.(jpe?g|gif|svg|png")$/i,
-      loaders: [
-        'url?limit=8192',
-        'image-webpack',
-      ],
-    }, {
-      test: /\.png$/,
-      loader: 'url-loader',
-      query: {
-        mimetype: 'image/png',
-      },
+      loader: 'babel-loader',
+      loader: 'svg-inline-loader',
+      loader: 'file-loader'
     }],
+    
   },
   externals: {
     Config: JSON.stringify(configuration),
